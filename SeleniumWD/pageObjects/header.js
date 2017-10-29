93926d0cda10ea1d3c87e6629d577406ac0b8960
Maxim Liubinski <maximliubinski@gmail.com>
2017-10-29 20:20:09 +0300

    initial commit

---
+++ SeleniumWD/pageObjects/header.js
@@ -0,0 +1,15 @@
+const webdriver = require('selenium-webdriver');
+
+class Header{
+    constructor() {
+        this.selebiumHQ = webdriver.By.css("a[title = 'Return to Selenium home page']");
+        this.projects = webdriver.By.css("a[title = 'Selenium Projects']");
+        this.download = webdriver.By.css("a[title = 'Get Selenium']");
+        this.documentation = webdriver.By.css("a[title = 'Technical references and guides']");
+        this.support = webdriver.By.css("a[title = 'Get help with Selenium']");
+        this.about = webdriver.By.css("a[title = 'Overview of Selenium']");
+        this.searchFild = webdriver.By.id("q");
+        this.searchGo = webdriver.By.id("submit");
+    }
+}
+module.exports = Header;
---
+++ SeleniumWD/runWD.js
@@ -0,0 +1,25 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+const Header = require('./pageObjects/header.js')
+
+let driver = new Builder()
+    .forBrowser('chrome')
+    .build();
+
+let header = new Header();
+
+driver.manage().window().maximize()
+    .then(() => driver.get('http://www.google.com/ncr'))
+    .then(() => driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN))
+    .then(() => driver.wait(until.titleIs('webdriver - Google Search'), 1000))
+    .then(() => driver.findElement(By.linkText("Selenium WebDriver")).click())
+    .then(() => driver.findElement(header.selebiumHQ).click())
+    .then(() => driver.findElement(header.projects).click())
+    .then(() => driver.findElement(header.download).click())
+    .then(() => driver.findElement(header.support).click())
+    .then(() => driver.findElement(header.about).click())
+    .then(() => driver.findElement(header.documentation).click());
+
+driver.executeScript('window.open("https://seleniumhq.github.io/selenium/docs/api/javascript/module/selenium-webdriver/");')
+    .then(() => driver.switchTo().frame('Selenium Documentation — Selenium Documentation'));
+
+//driver.quit();
